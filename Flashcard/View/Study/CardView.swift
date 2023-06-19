92ebaa3b399d46cde92a5b9e04edf8f4523a316b
yazuju-musa <97211329+yazuju-musa@users.noreply.github.com>
2023-06-20 06:35:21 +0800

    Fix card view

--- Flashcard/View/Study/CardView.swift
+++ Flashcard/View/Study/CardView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.dismiss) var dismiss
     @Environment(\.managedObjectContext) var viewContext
     @FetchRequest(sortDescriptors: []) var cards: FetchedResults<Card>
     
@@ -30,137 +31,120 @@ struct CardView: View {
     }
     
     var body: some View {
-        VStack {
-            DragBar()
-            ProgressView(learningCards: learningCards)
-            
-            ScrollView {
-                Spacer().frame(height: 20)
-                WordDefinitionView(word: "stick together", pronunciation: "stik ta'gedar")
+        GeometryReader { geometry in
+            VStack {
+                RoundedRectangle(cornerRadius: 5)
+                    .fill(Color(.systemGray4))
+                    .frame(width: 60, height: 8)
+                    .padding()
+                    .gesture(
+                        DragGesture()
+                            .onEnded { value in
+                                if value.translation.height > 50 {
+                                    dismiss()
+                                }
+                            }
+                    )
+                
+                ZStack(alignment: .leading) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.gray.opacity(0.5))
+                        .frame(height: 20)
+                    
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.blue)
+                        .frame(width: CGFloat(learningCards.filter { $0.isLearning }.count) / CGFloat(learningCards.count) * geometry.size.width, height: 20)
+                    
+                    HStack {
+                        Text("\(learningCards.filter { $0.isLearning }.count)")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                        Spacer()
+                        Text("\(learningCards.count - learningCards.filter { $0.isLearning }.count)")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
+                    .padding([.leading, .trailing])
+                }
+                
+                ScrollView {
+                    Spacer().frame(height: 20)
+                    
+                    VStack {
+                        Text(cards[index].text ?? "Unknown")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .foregroundColor(.primary)
+                        Text(cards[index].phoneticsArray.first?.text ?? "Unknown")
+                            .foregroundColor(.primary)
+                    }
                     .opacity(isWordVisible ? 1 : 0)
                     .animation(.easeIn(duration: 0.3), value: isWordVisible)
-                Spacer().frame(height: 20)
-                
-                ZStack {
-                    DefinitionView()
-                    Rectangle()
-                        .fill(Color.white)
-                        .opacity(isVStackVisible ? 0 : 1)
-                        .animation(.easeIn(duration: 0.3), value: isVStackVisible)
-                        .zIndex(1)
-                }
-
-                Spacer()
-            }
-            
-            HStack {
-                Button(action: {
-                    isVStackVisible = false
-                    isWordVisible = false
+                    Spacer().frame(height: 20)
                     
-                    print("index: \(index) cards: \(learningCards)")
-                    learningCards[index].isLearning = false
-                    index += 1
-                    
-                    if index == learningCards.count {
-                        // show complete view
+                    ZStack {
+                        DefinitionView()
+                        Rectangle()
+                            .fill(Color.white)
+                            .opacity(isVStackVisible ? 0 : 1)
+                            .animation(.easeIn(duration: 0.3), value: isVStackVisible)
+                            .zIndex(1)
                     }
                     
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                        isWordVisible = true
-                    }
-                }) {
-                    Text("Easy")
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+                    Spacer()
                 }
                 
-                Button(action: {
-                    isVStackVisible = false
-                    isWordVisible = false
-                    
-                    print("index: \(index) cards: \(learningCards)")
-                    let card = learningCards.remove(at: index)
-                    learningCards.append(card)
-                    
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                        isWordVisible = true
+                HStack {
+                    Button(action: {
+                        isVStackVisible = false
+                        isWordVisible = false
+                        
+                        print("index: \(index) cards: \(learningCards)")
+                        learningCards[index].isLearning = false
+                        index += 1
+                        
+                        if index == learningCards.count {
+                            // show complete view
+                        }
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                            isWordVisible = true
+                        }
+                    }) {
+                        Text("Easy")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
                     }
-                }) {
-                    Text("Hard")
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(.red)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+                    
+                    Button(action: {
+                        isVStackVisible = false
+                        isWordVisible = false
+                        
+                        print("index: \(index) cards: \(learningCards)")
+                        let card = learningCards.remove(at: index)
+                        learningCards.append(card)
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                            isWordVisible = true
+                        }
+                    }) {
+                        Text("Hard")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(.red)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
                 }
             }
         }
         .padding([.leading, .trailing])
-        .background(Color.white.ignoresSafeArea())
+        .background(Color.white.ignoresSafeArea(.all, edges: .top))
         .onTapGesture {
             isVStackVisible = true
-        }
-    }
-}
-
-struct DismissableSheet: View {
-    @Binding var showingSheet: Bool
-
-    var body: some View {
-        VStack {
-            DragBar()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.white.ignoresSafeArea())
-    }
-}
-
-struct DragBar: View {
-    @Environment(\.dismiss) var dismiss
-
-    var body: some View {
-        RoundedRectangle(cornerRadius: 5)
-            .fill(Color(.systemGray4))
-            .frame(width: 60, height: 8)
-            .padding()
-            .gesture(
-                DragGesture()
-                    .onEnded { value in
-                        if value.translation.height > 50 {
-                            dismiss()
-                        }
-                    }
-            )
-    }
-}
-
-struct ProgressView: View {
-    @State var learningCards: [LearningCard]
-    
-    var body: some View {
-        ZStack(alignment: .leading) {
-            RoundedRectangle(cornerRadius: 10)
-                .fill(Color.gray.opacity(0.5))
-                .frame(height: 20)
-            
-            RoundedRectangle(cornerRadius: 10)
-                .fill(Color.blue)
-                .frame(width: CGFloat(learningCards.filter { $0.isLearning }.count) / CGFloat(learningCards.count) * UIScreen.main.bounds.width, height: 20)
-            
-            HStack {
-                Text("\(learningCards.filter { $0.isLearning }.count)")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                Spacer()
-                Text("\(learningCards.count - learningCards.filter { $0.isLearning }.count)")
-                    .font(.headline)
-                    .foregroundColor(.white)
-            }
-            .padding([.leading, .trailing])
         }
     }
 }
@@ -175,22 +159,6 @@ struct DefinitionView: View {
             DefinitionDetailView(title: "Meaning 3: verb", description: "to continue to support each other", sample: "we must stick together and work as a team")
             Divider()
             SynonymView(synonyms: "seal, agree, collaborate, comply with, conspire, contribute, coordinate, further, help, participate, unite, uphold, accompany, marry, tie, attach, catch, fix, glue, hold")
-        }
-    }
-}
-
-struct WordDefinitionView: View {
-    let word: String
-    let pronunciation: String
-    
-    var body: some View {
-        VStack {
-            Text(word)
-                .font(.title3)
-                .fontWeight(.bold)
-                .foregroundColor(.primary)
-            Text(pronunciation)
-                .foregroundColor(.primary)
         }
     }
 }
@@ -265,5 +233,6 @@ struct CardView_Previews: PreviewProvider {
 
     static var previews: some View {
         CardView(showingCardView: $showingCardView, cardsToLearn: nil)
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
