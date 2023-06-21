84e60f71546cf5b16c8e00e99c593791dbb3882e
yazuju-musa <97211329+yazuju-musa@users.noreply.github.com>
2023-06-22 00:03:04 +0800

    Delete manually add feature

--- Flashcard/View/Add/AddView.swift
+++ Flashcard/View/Add/AddView.swift
@@ -16,48 +16,30 @@ struct AddCardView: View {
     @State private var cardText = ""
     @State private var isLoading = false
     @State private var progress: Float = 0.0
-    @State private var selection = 0
 
-    private let initialPlaceholder = "You can add cards using dictionary data. Multiple cards can be added by adding new lines. Both words and phrases are available.\n\npineapple\nstrawberry\ncherry\nblueberry\npeach\nplum\nRome was not built in a day\nAll that glitters is not gold\nEvery cloud has a silver lining"
+    private let initialPlaceholder = "You can add cards using dictionary data. Multiple cards can be added by adding new lines. Both words and phrases are available.\n\nExample:\npineapple\nstrawberry\ncherry\nblueberry\npeach\nplum\nRome was not built in a day\nAll that glitters is not gold\nEvery cloud has a silver lining"
     @ObservedObject var fetcher = WordFetcher()
     
     var body: some View {
         NavigationView {
             
             VStack {
-                
-                Section {
-                    Picker("Options", selection: $selection) {
-                        Text("Automatic").tag(0)
-                        Text("Manual").tag(1)
+                ZStack(alignment: .topLeading) {
+                    TextEditor(text: Binding(
+                        get: { self.isEditing ? self.cardText : self.initialPlaceholder },
+                        set: { self.cardText = $0 }
+                    ))
+                    .foregroundColor(isEditing ? .primary : .secondary)
+                    .onTapGesture {
+                        self.isEditing = true
                     }
-                    .pickerStyle(SegmentedPickerStyle())
-                    .padding()
                 }
-                
-                    if selection == 0 {
-                        ZStack(alignment: .topLeading) {
-                            TextEditor(text: Binding(
-                                get: { self.isEditing ? self.cardText : self.initialPlaceholder },
-                                set: { self.cardText = $0 }
-                            ))
-                            .foregroundColor(isEditing ? .primary : .secondary)
-                            .onTapGesture {
-                                self.isEditing = true
-                            }
-                        }
-                        .padding()
-                        .background(Color.white)
-                        .cornerRadius(10)
-                        .transition(.asymmetric(insertion: .move(edge: .leading), removal: .move(edge: .leading)))
-                        .animation(.default)
-                        .padding()
-                    } else {
-                        Rectangle()
-                            .background(Color.red)
-                            .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .trailing)))
-                            .animation(.default)
-                    }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(10)
+                .transition(.asymmetric(insertion: .move(edge: .leading), removal: .move(edge: .leading)))
+                .animation(.default)
+                .padding()
                 
                 Button(action: {
                     addCard()
