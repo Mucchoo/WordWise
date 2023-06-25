077b9b30ab871345e459adfa9b4d26b12d4248b0
yazuju-musa <97211329+yazuju-musa@users.noreply.github.com>
2023-06-25 17:38:29 +0800

    Add no cards view

--- Flashcard/View/Study/StudyView.swift
+++ Flashcard/View/Study/StudyView.swift
@@ -37,174 +37,197 @@ struct StudyView: View {
     }
     
     var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack {
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(Color(.systemGray6))
-                        .frame(height: 120)
-                        .overlay(
-                            HStack {
-                                InfoCard(systemName: "checkmark.circle.fill", count: cards.filter { $0.status == 0 }.count, title: "Learned", color: .blue)
-                                
-                                Divider().background(.gray)
-                                    .frame(height: 80)
-                                
-                                InfoCard(systemName: "pencil.circle.fill", count: cards.filter { $0.status == 1 }.count, title: "Learning", color: .red)
-                                
-                                Divider().background(.gray)
-                                    .frame(height: 80)
-                                
-                                InfoCard(systemName: "star.circle.fill", count: cards.filter { $0.status == 2 }.count, title: "New", color: .yellow)
-                            }
-                            .foregroundColor(.white)
-                        )
-                        .padding()
-                    
+        if cards.isEmpty {
+            VStack {
+                Spacer()
+                Text("No Cards")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
+                Image(systemName: "square.filled.on.square")
+                    .resizable()
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 120, height: 120)
+                Text("You have no cards yet.\nGo to the 'Add' tab \nto create your first one!")
+                    .font(.callout)
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
+                    .multilineTextAlignment(.center)
+                    .padding(.top)
+                Spacer()
+            }
+        } else {
+            NavigationView {
+                ScrollView {
                     VStack {
-                        Divider()
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color(.systemGray6))
+                            .frame(height: 120)
+                            .overlay(
+                                HStack {
+                                    InfoCard(systemName: "checkmark.circle.fill", count: cards.filter { $0.status == 0 }.count, title: "Learned", color: .blue)
+                                    
+                                    Divider().background(.gray)
+                                        .frame(height: 80)
+                                    
+                                    InfoCard(systemName: "pencil.circle.fill", count: cards.filter { $0.status == 1 }.count, title: "Learning", color: .red)
+                                    
+                                    Divider().background(.gray)
+                                        .frame(height: 80)
+                                    
+                                    InfoCard(systemName: "star.circle.fill", count: cards.filter { $0.status == 2 }.count, title: "New", color: .yellow)
+                                }
+                                .foregroundColor(.white)
+                            )
                             .padding()
                         
-                        HStack {
-                            Spacer().frame(width: 20)
-                            Text("Filter")
-                                .font(.title)
-                                .fontWeight(.bold)
-                            Spacer()
+                        VStack {
+                            Divider()
+                                .padding()
+                            
+                            HStack {
+                                Spacer().frame(width: 20)
+                                Text("Filter")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                Spacer()
+                            }
+                            
+                            HStack {
+                                StatusFilterButton(status: 0, text: "Learned", filterStatus: $filterStatus)
+                                StatusFilterButton(status: 1, text: "Learning", filterStatus: $filterStatus)
+                                StatusFilterButton(status: 2, text: "New", filterStatus: $filterStatus)
+                            }
+                            .padding([.leading, .trailing, .bottom])
+                            
+                            Button {
+                                showingCategorySheet.toggle()
+                            } label: {
+                                Text(selectedCategories.map { $0 }.joined(separator: ", "))
+                                    .fontWeight(.bold)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(.blue)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                            }
+                            .padding([.leading, .trailing])
+                            .sheet(isPresented: $showingCategorySheet) {
+                                List(cardCategories, id: \.self) { category in
+                                    HStack {
+                                        Text(category.name ?? "Unknown")
+                                        Spacer()
+                                        if selectedCategories.contains(category.name ?? "") {
+                                            Image(systemName: "checkmark.circle.fill")
+                                                .fontWeight(.bold)
+                                                .foregroundColor(.blue)
+                                        }
+                                    }
+                                    .contentShape(Rectangle())
+                                    .onTapGesture {
+                                        guard let categoryName = category.name else { return }
+                                        if selectedCategories.contains(categoryName) {
+                                            guard selectedCategories.count != 1 else { return }
+                                            selectedCategories.removeAll { $0 == category.name }
+                                        } else {
+                                            selectedCategories.append(categoryName)
+                                        }
+                                    }
+                                }
+                                .environment(\.editMode, .constant(EditMode.active))
+                                .presentationDetents([.medium, .large])
+                            }
+                            
+                            HStack {
+                                Text("Maximum Cards to Study")
+                                    .fontWeight(.bold)
+                                Spacer()
+                                NumberPicker(value: $maximumCardsToStudy, labelText: "cards", options: maximumCardOptions)
+                            }
+                            .padding([.leading, .trailing])
+                            
+                            HStack {
+                                Text("Failed Times more than")
+                                    .fontWeight(.bold)
+                                Spacer()
+                                NumberPicker(value: $failedTimesMoreThan, labelText: "or more times", options: failedTimeOptions)
+                            }
+                            .padding([.leading, .trailing])
                         }
                         
-                        HStack {
-                            StatusFilterButton(status: 0, text: "Learned", filterStatus: $filterStatus)
-                            StatusFilterButton(status: 1, text: "Learning", filterStatus: $filterStatus)
-                            StatusFilterButton(status: 2, text: "New", filterStatus: $filterStatus)
-                        }
-                        .padding([.leading, .trailing, .bottom])
-                        
-                        Button {
-                            showingCategorySheet.toggle()
-                        } label: {
-                            Text(selectedCategories.map { $0 }.joined(separator: ", "))
+                        Button(action: {
+                            guard cardsToStudy.count > 0 else { return }
+                            showingCardView = true
+                        }) {
+                            Text(cardsToStudy.count > 0 ? "Start Studying \(cardsToStudy.count) Cards" : "No Cards Available")
                                 .fontWeight(.bold)
                                 .padding()
                                 .frame(maxWidth: .infinity)
-                                .background(.blue)
+                                .background(cardsToStudy.count > 0 ? .blue : .gray)
                                 .foregroundColor(.white)
                                 .cornerRadius(10)
                         }
-                        .padding([.leading, .trailing])
-                        .sheet(isPresented: $showingCategorySheet) {
-                            List(cardCategories, id: \.self) { category in
-                                HStack {
-                                    Text(category.name ?? "Unknown")
-                                    Spacer()
-                                    if selectedCategories.contains(category.name ?? "") {
-                                        Image(systemName: "checkmark.circle.fill")
-                                            .fontWeight(.bold)
-                                            .foregroundColor(.blue)
-                                    }
-                                }
-                                .contentShape(Rectangle())
-                                .onTapGesture {
-                                    guard let categoryName = category.name else { return }
-                                    if selectedCategories.contains(categoryName) {
-                                        guard selectedCategories.count != 1 else { return }
-                                        selectedCategories.removeAll { $0 == category.name }
-                                    } else {
-                                        selectedCategories.append(categoryName)
-                                    }
-                                }
-                            }
-                            .environment(\.editMode, .constant(EditMode.active))
-                            .presentationDetents([.medium, .large])
+                        .disabled(cardsToStudy.count == 0)
+                        .padding()
+                        .fullScreenCover(isPresented: $showingCardView) {
+                            CardView(showingCardView: $showingCardView, cardsToStudy: cardsToStudy)
                         }
-                        
-                        HStack {
-                            Text("Maximum Cards to Study")
-                                .fontWeight(.bold)
-                            Spacer()
-                            NumberPicker(value: $maximumCardsToStudy, labelText: "cards", options: maximumCardOptions)
-                        }
-                        .padding([.leading, .trailing])
-                        
-                        HStack {
-                            Text("Failed Times more than")
-                                .fontWeight(.bold)
-                            Spacer()
-                            NumberPicker(value: $failedTimesMoreThan, labelText: "or more times", options: failedTimeOptions)
-                        }
-                        .padding([.leading, .trailing])
-                    }
-                    
-                    Button(action: {
-                        guard cardsToStudy.count > 0 else { return }
-                        showingCardView = true
-                    }) {
-                        Text(cardsToStudy.count > 0 ? "Start Studying \(cardsToStudy.count) Cards" : "No Cards Available")
-                            .fontWeight(.bold)
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .background(cardsToStudy.count > 0 ? .blue : .gray)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-                    .disabled(cardsToStudy.count == 0)
-                    .padding()
-                    .fullScreenCover(isPresented: $showingCardView) {
-                        CardView(showingCardView: $showingCardView, cardsToStudy: cardsToStudy)
-                    }
 
-                    VStack {
-                        Divider()
-                            .padding()
-                        
-                        HStack {
-                            Spacer().frame(width: 20)
-                            Text("\(cards.count) Cards")
-                                .font(.title)
-                                .fontWeight(.bold)
-                            Spacer()
-                        }
-                        
                         VStack {
-                            ForEach(cards) { card in
-                                CardListRowView(card: card)
+                            Divider()
+                                .padding()
+                            
+                            HStack {
+                                Spacer().frame(width: 20)
+                                Text("\(cards.count) Cards")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                Spacer()
+                            }
+                            
+                            VStack {
+                                ForEach(cards) { card in
+                                    CardListRowView(card: card)
+                                }
                             }
                         }
+                        
+                        Spacer()
                     }
-                    
-                    Spacer()
                 }
+                .navigationBarTitle("Study", displayMode: .large)
             }
-            .navigationBarTitle("Study", displayMode: .large)
-        }
-        .onAppear {
-            cards.forEach { card in
-                guard card.category == nil else { return }
-                card.category = cardCategories.first?.name
-                PersistenceController.shared.saveContext()
+            .onAppear {
+                cards.forEach { card in
+                    guard card.category == nil else { return }
+                    card.category = cardCategories.first?.name
+                    PersistenceController.shared.saveContext()
+                }
+                
+                guard isFirstAppearance else { return }
+                selectedCategories = cardCategories.map { $0.name ?? "" }
+                isFirstAppearance = false
             }
-            
-            guard isFirstAppearance else { return }
-            selectedCategories = cardCategories.map { $0.name ?? "" }
-            isFirstAppearance = false
-        }
-        .onChange(of: failedTimesMoreThan) { _ in
-            updateCardsToStudy()
-        }
-        .onChange(of: selectedCategories) { _ in
-            updateCardsToStudy()
-        }
-        .onChange(of: maximumCardsToStudy) { _ in
-            updateCardsToStudy()
-        }
-        .onChange(of: filterStatus) { _ in
-            updateCardsToStudy()
-        }
-        .onReceive(NotificationCenter.default.publisher(for: .NSManagedObjectContextObjectsDidChange, object: viewContext)) { _ in
-            updateCardsToStudy()
-            
-            if selectedCategories.isEmpty {
-                selectedCategories = Array(cardCategories).map { $0.name ?? "" }
+            .onChange(of: failedTimesMoreThan) { _ in
+                updateCardsToStudy()
+            }
+            .onChange(of: selectedCategories) { _ in
+                updateCardsToStudy()
+            }
+            .onChange(of: maximumCardsToStudy) { _ in
+                updateCardsToStudy()
+            }
+            .onChange(of: filterStatus) { _ in
+                updateCardsToStudy()
+            }
+            .onReceive(NotificationCenter.default.publisher(for: .NSManagedObjectContextObjectsDidChange, object: viewContext)) { _ in
+                updateCardsToStudy()
+                
+                if selectedCategories.isEmpty {
+                    selectedCategories = Array(cardCategories).map { $0.name ?? "" }
+                }
             }
         }
     }
