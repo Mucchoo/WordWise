636316d54357b0528420d818270396186d8359e7
yazuju-musa <97211329+yazuju-musa@users.noreply.github.com>
2023-07-02 12:17:13 +0800

    Use BackgroundView

--- VocabBuilder/View/Account/AccountView.swift
+++ VocabBuilder/View/Account/AccountView.swift
@@ -120,55 +120,9 @@ struct AccountView: View {
                     .padding(.vertical, 10)
                 }
             }
-            .background {
-                ZStack {
-                    Color(UIColor.systemGroupedBackground)
-                        .edgesIgnoringSafeArea(.all)
-                    ClubbedView(initialAnimation: $initialAnimation)
-                        .edgesIgnoringSafeArea(.all)
-                }
-            }
+            .background(BackgroundView(initialAnimation: $initialAnimation))
             .navigationBarTitle("Account", displayMode: .large)
         }
-    }
-    
-    @ViewBuilder
-    func ClubbedView(initialAnimation: Binding<Bool>) -> some View {
-        Rectangle()
-            .fill(.linearGradient(colors: [Color("Teal"), Color("Mint")], startPoint: .top, endPoint: .bottom))
-            .mask {
-                TimelineView(.animation(minimumInterval: 20, paused: false)) { _ in
-                    ZStack {
-                        Canvas { context, size in
-                            context.addFilter(.alphaThreshold(min: 0.5, color: .yellow))
-                            context.addFilter(.blur(radius: 30))
-                            context.drawLayer { ctx in
-                                for index in 1...30 {
-                                    if let resolvedView = context.resolveSymbol(id: index) {
-                                        ctx.draw(resolvedView, at: CGPoint(x: size.width / 2, y: size.height / 2))
-                                    }
-                                }
-                            }
-                        } symbols: {
-                            ForEach(1...30, id: \.self) { index in
-                                let offset = CGSize(width: .random(in: -300...300), height: .random(in: -500...500))
-                                ClubbedRoundedRectangle(offset: offset, initialAnimation: $initialAnimation.wrappedValue, width: 100, height: 100, corner: 50)
-                                    .tag(index)
-                            }
-                        }
-                    }
-                }
-            }
-        .contentShape(Rectangle())
-    }
-    
-    @ViewBuilder
-    func ClubbedRoundedRectangle(offset: CGSize, initialAnimation: Bool, width: CGFloat, height: CGFloat, corner: CGFloat) -> some View {
-        RoundedRectangle(cornerRadius: corner, style: .continuous)
-            .fill(.white)
-            .frame(width: width, height: height)
-            .offset(x: initialAnimation ? offset.width : 0, y: initialAnimation ? offset.height : 0)
-            .animation(.easeInOut(duration: 20), value: offset)
     }
 }
 
