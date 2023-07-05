4edf183c0398a1df96b5b2626d82ca385a9873c2
yazuju-musa <97211329+yazuju-musa@users.noreply.github.com>
2023-07-05 12:44:59 +0800

    Fix CardListView icon colors

--- VocabBuilder/View/CardList/CardListView.swift
+++ VocabBuilder/View/CardList/CardListView.swift
@@ -99,7 +99,7 @@ struct CardListView: View {
                                         }) {
                                             HStack{
                                                 Image(systemName: card.status == 0 ? "checkmark.circle.fill" : card.status == 1 ? "flame.circle.fill" : "star.circle.fill")
-                                                    .foregroundColor(card.status == 0 ? .blue : card.status == 1 ? .red : .yellow)
+                                                    .foregroundColor(card.status == 0 ? Color("Navy") : card.status == 1 ? Color("Blue") : Color("Teal"))
                                                     .font(.system(size: 16))
                                                     .fontWeight(.black)
                                                     .frame(width: 20, height: 20, alignment: .center)
