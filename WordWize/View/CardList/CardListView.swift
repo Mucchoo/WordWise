1a2b148af11cd4369de3c0057bc39b9008456a0b
yazuju-musa <97211329+yazuju-musa@users.noreply.github.com>
2023-08-14 09:22:49 +0800

    Delete unused code from CardListView

--- WordWize/View/CardList/CardListView.swift
+++ WordWize/View/CardList/CardListView.swift
@@ -9,26 +9,10 @@ import SwiftUI
 
 struct CardListView: View {
     @EnvironmentObject var dataViewModel: DataViewModel
-    @FocusState var isFocused: Bool
-    
-    @State private var WordWizes = [String]()
-    @State private var isEditing = false
-    @State private var isLoading = false
-    @State private var progress: Float = 0.0
-    @State private var pickerSelected = ""
-    @State private var showingAlert = false
-    @State private var textFieldInput = ""
-    @State private var showingFetchFailedAlert = false
-    @State private var fetchFailedWords: [String] = []
-    @State private var isFirstAppearance = true
-    @State private var showingCategorySheet = false
     
     @State var categoryName = ""
     @State private var searchBarText = ""
     @State private var cardList: [Card] = []
-
-    @State private var filterCategories: [String] = []
-    @State private var filterStatus: [Int16]  = [0, 1, 2]
         
     var body: some View {
         VStack {
@@ -47,27 +31,15 @@ struct CardListView: View {
         }
         .background(BackgroundView())
         .navigationBarTitle(categoryName, displayMode: .large)
-        .onAppear {
-            guard isFirstAppearance else { return }
-            filterCategories = dataViewModel.categories.map { $0.name ?? "" }
-            isFirstAppearance = false
-        }
         .onReceive(dataViewModel.$cards) { _ in
-            updateCardList()
-        }
-        .onChange(of: filterCategories) { _ in
-            updateCardList()
-        }
-        .onChange(of: filterStatus) { _ in
             updateCardList()
         }
     }
     
     private func updateCardList() {
         let filteredCards = dataViewModel.cards.filter { card in
-            let statusFilter = filterStatus.contains { $0 == card.status }
-            let categoryFilter = filterCategories.contains { $0 == card.category }
-            return statusFilter && categoryFilter
+            let categoryFilter = card.category == categoryName
+            return categoryFilter
         }
         cardList = filteredCards
     }
