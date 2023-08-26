01b442f42b5d1451bdc0e26bc48e34d8e7f2ad31
yazuju-musa <97211329+yazuju-musa@users.noreply.github.com>
2023-08-27 01:24:15 +0800

    Delete MasteryRateCountView

--- WordWize/View/Global/MasteryRateCountsView.swift
+++ WordWize/View/Global/MasteryRateCountsView.swift
@@ -13,16 +13,65 @@ struct MasteryRateCountsView: View {
     var body: some View {
         VStack(spacing: 0) {
             HStack(spacing: 0) {
-                MasteryRateCountView(masteryRate: .zero, category: $category)
-                MasteryRateCountView(masteryRate: .twentyFive, category: $category)
+                RateBar(masteryRate: .zero, category: $category)
+                RateBar(masteryRate: .twentyFive, category: $category)
             }
             HStack(spacing: 0) {
-                MasteryRateCountView(masteryRate: .fifty, category: $category)
-                MasteryRateCountView(masteryRate: .seventyFive, category: $category)
+                RateBar(masteryRate: .fifty, category: $category)
+                RateBar(masteryRate: .seventyFive, category: $category)
             }
         }
         .cornerRadius(20)
         .clipped()
+    }
+}
+
+private struct RateBar: View {
+    @EnvironmentObject var dataViewModel: DataViewModel
+    var masteryRate: MasteryRate
+    @Binding var category: String
+    
+    var cardCount: Int {
+        let categoryCards = dataViewModel.cards.filter { $0.category == category }
+        let rateCards = categoryCards.filter { $0.rate == masteryRate }
+        return rateCards.count
+    }
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 4) {
+            Text(masteryRate.stringValue() + "%:")
+                .font(.subheadline)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .padding(.top, 2)
+            Text("\(cardCount)")
+                .font(.title3)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            Text("cards")
+                .font(.caption2)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .padding(.top, 6)
+        }
+        .frame(height: 60)
+        .frame(maxWidth: .infinity)
+        .background(LinearGradient(colors: getColors(), startPoint: .topLeading, endPoint: .bottomTrailing))
+    }
+    
+    func getColors() -> [Color] {
+        switch masteryRate {
+        case .zero:
+            return [.black, .navy]
+        case .twentyFive:
+            return [.navy, .ocean]
+        case .fifty:
+            return [.navy, .ocean]
+        case .seventyFive:
+            return [.ocean, .teal]
+        case .oneHundred:
+            return []
+        }
     }
 }
 
