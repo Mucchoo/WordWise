80c2bfee238a91b6c478bd8b311eea0ead3eae81
yazuju-musa <97211329+yazuju-musa@users.noreply.github.com>
2023-08-27 16:22:11 +0800

    Make AccountView subview structs private

--- WordWize/View/AccountTab/AccountView.swift
+++ WordWize/View/AccountTab/AccountView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import UIKit
+import MessageUI
 
 struct AccountView: View {
     @EnvironmentObject var dataViewModel: DataViewModel
@@ -18,7 +20,7 @@ struct AccountView: View {
     @State var showingShareSheet = false
     @AppStorage("nativeLanguage") private var nativeLanguage = "JA"
 
-    @State private var mailData = Email(subject: "Feedback", recipients: ["yazujumusa@gmail.com"], message: "\n\n\n\n\nーーーーーーーーーーーーーーーーー\nPlease write your feedback above this line. Thank you!")
+    @State private var mailData = MailView.Email(subject: "Feedback", recipients: ["yazujumusa@gmail.com"], message: "\n\n\n\n\nーーーーーーーーーーーーーーーーー\nPlease write your feedback above this line. Thank you!")
     private var productURL = URL(string: "https://itunes.apple.com/jp/app/id1628829703?mt=8")!
     
     var body: some View {
@@ -112,6 +114,67 @@ struct AccountView: View {
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }
+}
+
+private struct MailView: UIViewControllerRepresentable {
+    typealias Callback = ((Result<MFMailComposeResult, Error>) -> Void)?
+    
+    struct Email {
+      let subject: String
+      let recipients: [String]?
+      let message: String
+    }
+    
+    @Environment(\.presentationMode) var presentation
+    @Binding var data: Email
+    let callback: Callback
+    
+    class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+        @Binding var presentation: PresentationMode
+        @Binding var data: Email
+        let callback: Callback
+        
+        init(presentation: Binding<PresentationMode>, data: Binding<Email>, callback: Callback) {
+            _presentation = presentation
+            _data = data
+            self.callback = callback
+        }
+        
+        func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+            if let error = error {
+                callback?(.failure(error))
+            } else {
+                callback?(.success(result))
+            }
+            $presentation.wrappedValue.dismiss()
+        }
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MailView>) -> MFMailComposeViewController {
+        let vc = MFMailComposeViewController()
+        vc.mailComposeDelegate = context.coordinator
+        vc.setSubject(data.subject)
+        vc.setToRecipients(data.recipients)
+        vc.setMessageBody(data.message, isHTML: false)
+        return vc
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(presentation: presentation, data: $data, callback: callback)
+    }
+    
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController, context: UIViewControllerRepresentableContext<MailView>) {}
+}
+
+private struct ActivityViewController: UIViewControllerRepresentable {
+    var shareItems: [Any]
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityViewController>) -> UIActivityViewController {
+        let controller = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: UIViewControllerRepresentableContext<ActivityViewController>) {}
 }
 
 #Preview {
