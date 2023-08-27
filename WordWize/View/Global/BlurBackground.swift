59288e6083c8ca883954a04529fa7cb147f12b2a
yazuju-musa <97211329+yazuju-musa@users.noreply.github.com>
2023-08-27 16:24:10 +0800

    Make TransparentBlurView private

--- WordWize/View/Global/BlurBackground.swift
+++ WordWize/View/Global/BlurBackground.swift
@@ -24,3 +24,48 @@ struct BlurBackground: ViewModifier {
             .padding()
     }
 }
+
+private struct TransparentBlurView: UIViewRepresentable {
+    var removeAllLayers = false
+    func makeUIView(context: Context) -> TransparentBlurViewHelper {
+        return TransparentBlurViewHelper(removeAllFilters: removeAllLayers)
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        DispatchQueue.main.async {
+            if let backdropLayer = uiView.layer.sublayers?.first {
+                if removeAllLayers {
+                    backdropLayer.filters = []
+                } else {
+                    backdropLayer.filters?.removeAll(where: { filter in
+                        String(describing: filter) != "gaussianBlur"
+                    })
+                }
+            }
+        }
+    }
+}
+
+private class TransparentBlurViewHelper: UIVisualEffectView {
+    init(removeAllFilters: Bool) {
+        super.init(effect: UIBlurEffect(style: .systemUltraThinMaterial))
+        
+        if let backdropLayer = layer.sublayers?.first {
+            if removeAllFilters {
+                backdropLayer.filters = []
+            } else {
+                backdropLayer.filters?.removeAll(where: { filter in
+                    String(describing: filter) != "gaussianBlur"
+                })
+            }
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        
+    }
+}
