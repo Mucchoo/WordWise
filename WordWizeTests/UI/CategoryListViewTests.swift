296a16a693a830878603d2ac8e7011b76b2bcba7
Musa Yazici <musa@Musas-MacBook-Air.local>
2023-09-10 04:38:13 +0800

    Add CategoryListViewTests

---
+++ WordWizeTests/UI/CategoryListViewTests.swift
@@ -0,0 +1,52 @@
+//
+//  CategoryListViewTests.swift
+//  WordWizeTests
+//
+//  Created by Musa Yazici on 9/10/23.
+//
+
+import XCTest
+import ViewInspector
+import SwiftUI
+@testable import WordWize
+
+class CategoryListViewTests: XCTestCase {
+    
+    var vm: CategoryListViewModel!
+    var sut: CategoryListView!
+    
+    override func setUp() {
+        super.setUp()
+        vm = .init(container: .mock())
+        sut = .init(vm: vm)
+    }
+    
+    override func tearDown() {
+        vm = nil
+        sut = nil
+        super.tearDown()
+    }
+
+    func testEmptyState() throws {
+        vm.container.appState.categories = []
+        let _ = try sut.inspect().find(NoCardView.self)
+    }
+    
+    func testCategoryRowCount() throws {
+        let rowCount = try sut.inspect().navigationView().scrollView().vStack().forEach(0).count
+        XCTAssertEqual(rowCount, 1)
+    }
+    
+    func testCategoryRow() throws {
+        let categoryRow = try sut.inspect().navigationView().scrollView().vStack().forEach(0)[0]
+                
+        let menu = try categoryRow.zStack().hStack(1).vStack(1).menu(1)
+        let _ = try menu.labelView().image(0)
+        let _ = try menu.button(0)
+        let _ = try menu.button(1)
+
+        let displayedName = try categoryRow.zStack().navigationLink(0).labelView().vStack(0).hStack(0).text(0).string()
+        XCTAssertEqual(displayedName, MockHelper.shared.mockCategory)
+    }
+}
+
