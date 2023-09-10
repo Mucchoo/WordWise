7af26b3b412d4421701e2353cb68bf524a9c05de
Musa Yazici <musa@Musas-MacBook-Air.local>
2023-09-11 01:07:45 +0800

    Add RealNetworkServiceTests

--- WordWizeTests/Service/RealNetworkServiceTests.swift
+++ WordWizeTests/Service/RealNetworkServiceTests.swift
@@ -17,7 +17,7 @@ class RealNetworkServiceTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        mockSession = .init(configuration: .ephemeral)
+        mockSession = .mockedResponsesOnly
         sut = RealNetworkService(session: mockSession)
         cancellables = []
     }
@@ -29,53 +29,87 @@ class RealNetworkServiceTests: XCTestCase {
         super.tearDown()
     }
     
+    func testFetchTranslations() {
+        let mockTexts = ["hello", "world"]
+        XCTAssertNoThrow(
+            sut.fetchTranslations(mockTexts)
+            .sink(receiveCompletion: { _ in }, receiveValue: { _ in })
+            .store(in: &cancellables)
+        )
+    }
+    
     func testFetchDefinitionsAndImages() {
         let mockContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
         let card = Card(context: mockContext)
-        sut.fetchDefinitionsAndImages(card: card, context: mockContext)
+        
+        XCTAssertNoThrow(
+            sut.fetchDefinitionsAndImages(card: card, context: mockContext)
+            .sink(receiveCompletion: { _ in }, receiveValue: { _ in })
+            .store(in: &cancellables)
+        )
     }
     
     func testDownloadAndSetImages() {
         let mockContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
         let card = Card(context: mockContext)
-        sut.downloadAndSetImages(card: card, context: mockContext, imageUrls: [])
+        
+        XCTAssertNoThrow(sut.downloadAndSetImages(card: card, context: mockContext, imageUrls: []))
     }
     
     func testRetryFetchingImages() {
         let mockContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
         let mockCard = Card(context: mockContext)
-        sut.retryFetchingImages(card: mockCard, context: mockContext)
+        
+        XCTAssertNoThrow(
+            sut.retryFetchingImages(card: mockCard, context: mockContext)
+            .sink(receiveCompletion: { _ in }, receiveValue: { _ in })
+            .store(in: &cancellables)
+        )
     }
     
     func testFetchDefinitionsFromMerriamWebsterAPI() {
-        sut.fetchDefinitionsFromMerriamWebsterAPI(word: "")
+        XCTAssertNoThrow(
+            sut.fetchDefinitionsFromMerriamWebsterAPI(word: "")
+            .sink(receiveCompletion: { _ in }, receiveValue: { _ in })
+            .store(in: &cancellables)
+        )
     }
     
     func testFetchDefinitionsFromFreeAPI() {
-        sut.fetchDefinitionsFromFreeAPI(word: "")
+        XCTAssertNoThrow(
+            sut.fetchDefinitionsFromFreeAPI(word: "")
+            .sink(receiveCompletion: { _ in }, receiveValue: { _ in })
+            .store(in: &cancellables)
+        )
     }
     
     func testFetchImages() {
-        sut.fetchImages(word: "")
-    }
-    
-    func testFetchDefinitions() {
-        sut.fetchDefinitions(word: "")
-    }
-    
-    func testFetchTranslations() {
-        let mockTexts = ["hello", "world"]
-        sut.fetchTranslations(mockTexts)
+        XCTAssertNoThrow(
+            sut.fetchImages(word: "")
+            .sink(receiveCompletion: { _ in }, receiveValue: { _ in })
+            .store(in: &cancellables)
+        )
     }
     
     func testConvertMerriamWebsterDefinition() {
-        sut.convertMerriamWebsterDefinition(word: "", data: [])
+        let mockData: [MerriamWebsterDefinition] = []
+        XCTAssertNoThrow(_ = sut.convertMerriamWebsterDefinition(word: "", data: mockData))
     }
     
     func testSetDefinitionData() {
         let mockContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
         let card = Card(context: mockContext)
         let mockData: WordDefinition = .init(word: "", phonetic: "", phonetics: [], origin: "", meanings: [])
-        sut.setDefinitionData(card: card, context: mockContext, data: mockData)
+
+        XCTAssertNoThrow(sut.setDefinitionData(card: card, context: mockContext, data: mockData))
+    }
+}
+
+extension URLSession {
+    static var mockedResponsesOnly: URLSession {
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = 1
+        configuration.timeoutIntervalForResource = 1
+        return URLSession(configuration: configuration)
     }
 }
