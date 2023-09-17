e9e4fcb74778bdbfdce2c9881ac81527be4a5549
Musa Yazici <musa@Musas-MacBook-Air.local>
2023-09-17 12:41:55 +0800

    Save APIKeys in Keys.env

--- WordWize/Helper/Keys.swift
+++ WordWize/Helper/Keys.swift
@@ -7,7 +7,7 @@
 import Foundation
 
 struct Keys {
-    static let pixabayApiKey = ""
-    static let deepLApiKey = ""
-    static let merriamWebsterApiKey = ""
+    static let pixabayApiKey = ProcessInfo.processInfo.environment["PIXABAY_API_KEY"] ?? ""
+    static let deepLApiKey = ProcessInfo.processInfo.environment["DEEPL_API_KEY"] ?? ""
+    static let merriamWebsterApiKey = ProcessInfo.processInfo.environment["MERRIAM_WEBSTER_API_KEY"] ?? ""
 }
--- WordWize/WordWizeApp.swift
+++ WordWize/WordWizeApp.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 @main
 struct WordWizeApp: App {
+    
+    init() {
+        loadEnvironmentVariables()
+    }
+    
     var body: some Scene {
         let container = DIContainer(
             appState: AppState(),
@@ -17,6 +22,22 @@ struct WordWizeApp: App {
         
         WindowGroup {
             ContentView(container: container)
+        }
+    }
+    
+    private func loadEnvironmentVariables() {
+        guard let envPath = Bundle.main.path(forResource: "Keys.env", ofType: nil),
+              let contents = try? String(contentsOfFile: envPath) else { return }
+        
+        let lines = contents.split(separator: "\n")
+        for line in lines {
+            let parts = line.split(separator: "=", maxSplits: 1)
+            guard parts.count == 2 else { continue }
+            
+            let key = String(parts[0])
+            let value = String(parts[1])
+            print("Setting \(key) to \(value)")
+            setenv(key, value, 1)
         }
     }
 }
